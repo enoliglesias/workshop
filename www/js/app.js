a4a71ea20d5050f2eea49e693db000b6adc88da5
Enol Iglesias <enoliglesias@gmail.com>
2015-05-08 23:36:32 +0200

    Remove useless code in app.js

--- www/js/app.js
+++ www/js/app.js
@@ -3,11 +3,12 @@
 
     /* ---------------------------------- Local Variables ---------------------------------- */
     var service = new EmployeeService();
-    var homeTpl = Handlebars.compile($("#home-tpl").html());
-    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
+    HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+    EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
     service.initialize().done(function () {
-        renderHomeView();
+      $('body').html(new HomeView(service).render().$el);
     });
+
 
     /* --------------------------------- Event Registration -------------------------------- */
     document.addEventListener('deviceready', function () {
@@ -25,15 +26,4 @@
 
 
     /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-      service.findByName($('.search-key').val()).done(function (employees) {
-        $('.content').html(employeeListTpl(employees));
-      });
-    }
-
-    function renderHomeView() {
-      $('body').html(homeTpl());
-      $('.search-key').on('keyup', findByName);
-    }
-
 }());
